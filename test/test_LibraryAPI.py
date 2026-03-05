679b3e192cc8d38ebef9577e838eb869f062653b
ramananr49 <venketramanan@live.in>
2026-03-05 22:32:10 +0530

    Handling Get by ID API using PW_Py in API Testing

--- test/test_LibraryAPI.py
+++ test/test_LibraryAPI.py
@@ -1,6 +1,7 @@
+import pytest
 from playwright.sync_api import Playwright
 
-
+@pytest.mark.skip
 def test_get_book_api(playwright: Playwright):
     request_context = playwright.request.new_context(base_url="http://216.10.245.166")
     res = request_context.get("/Library/GetBook.php?AuthorName=Hamilton")
@@ -12,3 +13,9 @@ def test_get_book_api(playwright: Playwright):
             assert data[i]["isbn"] == "RRAY"
             print(data[i]["book_name"])
             print(data[i]["aisle"])
+
+def test_get_book_by_id_api(playwright: Playwright):
+    request_context = playwright.request.new_context(base_url="http://216.10.245.166")
+    res = request_context.get("/Library/GetBook.php?ID=RRAY21")
+    print(res.status)
+    print(res.json())
